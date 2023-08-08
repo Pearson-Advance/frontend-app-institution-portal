a1323b24a9e106fa9a2d92515704eda7b5780240
Sergio Valero <sergio.valero@edunext.co>
2023-08-08 18:03:18 -0400

    refactor: improve Footer component

--- src/features/Main/Footer/index.jsx
+++ src/features/Main/Footer/index.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
 import './index.scss';
 
-export const Footer = () => (<footer><ul className="footer-links"><li><a href="https://pearsonadvance.com/privacy-notice/">Privacy Policy</a></li><li><a href="https://pearsonadvance.com/tos/">Terms of Service</a></li></ul></footer>);
+export const Footer = () => (
+  <footer>
+    <ul className="footer-links">
+      <li>
+        <a href="https://pearsonadvance.com/privacy-notice/">
+          Privacy Policy
+        </a>
+      </li>
+      <li>
+        <a href="https://pearsonadvance.com/tos/">
+          Terms of Service
+        </a>
+      </li>
+    </ul>
+  </footer>
+);
