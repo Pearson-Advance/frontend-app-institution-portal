10604ed7a6ed1b6d53959b37111ec84c140fa897
Sergio Valero <sergio.valero@edunext.co>
2023-08-10 09:41:14 -0400

    refactor: move svg elements to separete files

--- src/features/Main/Header/index.jsx
+++ src/features/Main/Header/index.jsx
@@ -2,6 +2,7 @@ import { AppContext } from '@edx/frontend-platform/react';
 import { getConfig } from '@edx/frontend-platform';
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import ArrowIcon from '../../../assets/images/arrow_icon.svg';
 import './index.scss';
 
 export const Header = ({ isAccountMenuOpen, setIsAccountMenuOpen }) => {
@@ -20,9 +21,7 @@ export const Header = ({ isAccountMenuOpen, setIsAccountMenuOpen }) => {
           <button type="button" className="button-unstyled" aria-expanded={isAccountMenuOpen} onClick={handleAccountMenuToggle}>
             <span className="avatar">{usernameFirstLetter}</span>
             <span className="icon-18">
-              <svg xmlnsXlink="http://www.w3.org/1999/xlink" width="18" height="18" viewBox="0 0 18 18" fill="none">
-                <path fillRule="evenodd" clipRule="evenodd" d="M5.99434 7H12.0057C12.5548 7 13 7.44827 13 8.00124C13 8.26845 12.8939 8.52458 12.7054 8.71261L9.69972 11.7101C9.31219 12.0966 8.68781 12.0966 8.30028 11.7101L5.29463 8.71261C4.90445 8.32349 4.90142 7.68955 5.28786 7.29667C5.47461 7.10681 5.72897 7 5.99434 7Z" fill="#020917" />
-              </svg>
+              <img src={ArrowIcon} alt="icon" />
             </span>
           </button>
           {isAccountMenuOpen && (
