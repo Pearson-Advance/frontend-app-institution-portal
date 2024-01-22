aca99d08d056567d61a2a0ce276eb3676e72fe98
Aura Alba <aura.alba@edunext.co>
2024-01-22 12:36:48 -0500

    feat: update students table

--- src/features/Students/StudentsTable/_test_/columns.test.jsx
+++ src/features/Students/StudentsTable/_test_/columns.test.jsx
@@ -5,39 +5,27 @@ describe('getColumns', () => {
     const columns = getColumns();
 
     expect(columns).toBeInstanceOf(Array);
-    expect(columns).toHaveLength(8);
+    expect(columns).toHaveLength(5);
 
     const [
       nameColumn,
-      courseNameColumn,
-      courseIdColumn,
+      emailColumn,
       classNameColumn,
       ClassIdColumn,
-      instructorsColumn,
-      statusColumn,
       examReadyColumn,
     ] = columns;
 
     expect(nameColumn).toHaveProperty('Header', 'Student');
     expect(nameColumn).toHaveProperty('accessor', 'learnerName');
 
-    expect(courseNameColumn).toHaveProperty('Header', 'Course');
-    expect(courseNameColumn).toHaveProperty('accessor', 'courseName');
-
-    expect(courseIdColumn).toHaveProperty('Header', 'Course Id');
-    expect(courseIdColumn).toHaveProperty('accessor', 'courseId');
+    expect(emailColumn).toHaveProperty('Header', 'Email');
+    expect(emailColumn).toHaveProperty('accessor', 'learnerEmail');
 
     expect(classNameColumn).toHaveProperty('Header', 'Class Name');
     expect(classNameColumn).toHaveProperty('accessor', 'className');
 
     expect(ClassIdColumn).toHaveProperty('Header', 'Class Id');
     expect(ClassIdColumn).toHaveProperty('accessor', 'classId');
-
-    expect(instructorsColumn).toHaveProperty('Header', 'Instructor');
-    expect(instructorsColumn).toHaveProperty('accessor', 'instructors');
-
-    expect(statusColumn).toHaveProperty('Header', 'Status');
-    expect(statusColumn).toHaveProperty('accessor', 'status');
 
     expect(examReadyColumn).toHaveProperty('Header', 'Exam Ready');
     expect(examReadyColumn).toHaveProperty('accessor', 'examReady');
--- src/features/Students/StudentsTable/columns.jsx
+++ src/features/Students/StudentsTable/columns.jsx
@@ -1,6 +1,4 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { Badge } from 'react-paragon-topaz';
 
 const getColumns = () => [
   {
@@ -8,12 +6,8 @@ const getColumns = () => [
     accessor: 'learnerName',
   },
   {
-    Header: 'Course',
-    accessor: 'courseName',
-  },
-  {
-    Header: 'Course Id',
-    accessor: 'courseId',
+    Header: 'Email',
+    accessor: 'learnerEmail',
   },
   {
     Header: 'Class Name',
@@ -24,33 +18,6 @@ const getColumns = () => [
     accessor: 'classId',
   },
   {
-    Header: 'Instructor',
-    accessor: 'instructors',
-    Cell: ({ row }) => (
-      <ul style={{ listStyleType: 'none', paddingLeft: 0 }}>
-        {row.values.instructors.map(instructor => <li key={instructor}>{instructor}</li>)}
-      </ul>
-    ),
-  },
-  {
-    Header: 'Status',
-    accessor: 'status',
-    Cell: ({ row }) => {
-      switch (row.values.status) {
-        case 'Active':
-          return <Badge variant="success" light>Active</Badge>;
-        case 'Inactive':
-          return <Badge variant="secondary" light>Inactive</Badge>;
-        case 'Expired':
-          return <Badge variant="danger" light>Expired</Badge>;
-        case 'Pending':
-          return <Badge variant="warning" light>Pending</Badge>;
-        default:
-          return null;
-      }
-    },
-  },
-  {
     Header: 'Exam Ready',
     accessor: 'examReady',
     Cell: ({ row }) => (row.values.examReady ? 'yes' : 'no'),
@@ -58,6 +25,6 @@ const getColumns = () => [
 ];
 
 // We don't need to show ccxId column but we need it to use handleStudentsActions.
-const hideColumns = { hiddenColumns: ['classId', 'courseId'] };
+const hideColumns = { hiddenColumns: ['classId'] };
 
 export { hideColumns, getColumns };
