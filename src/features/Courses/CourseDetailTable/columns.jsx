7cf32067df33708dfa11ed0413aad6faf14460d3
Nicolas Jiménez <nicolas.jimenez@edunext.co>
2024-06-04 12:02:34 -0500

    feat: dropdown option in course detail table

--- src/features/Courses/CourseDetailTable/columns.jsx
+++ src/features/Courses/CourseDetailTable/columns.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { useParams, Link } from 'react-router-dom';
+import {
+  Dropdown, useToggle, IconButton, Icon,
+} from '@edx/paragon';
+import { Badge } from 'react-paragon-topaz';
+import { MoreHoriz } from '@edx/paragon/icons';
+import { getConfig } from '@edx/frontend-platform';
 
-import { Badge, Button } from 'react-paragon-topaz';
-import AssignInstructors from 'features/Instructors/AssignInstructors';
-
-import { fetchClassesData } from 'features/Classes/data/thunks';
-import { updateClassSelected } from 'features/Instructors/data/slice';
-
-import { initialPage } from 'features/constants';
 import { formatUTCDate } from 'helpers';
+
+import AddClass from 'features/Courses/AddClass';
+
+import 'assets/global.scss';
 
 const columns = [
   {
@@ -32,22 +34,6 @@ const columns = [
     Header: 'Instructor',
     accessor: 'instructors',
     Cell: ({ row }) => {
-      const { courseId } = useParams();
-      const [isModalOpen, setIsModalOpen] = useState(false);
-
-      const dispatch = useDispatch();
-      const institution = useSelector((state) => state.main.selectedInstitution);
-
-      const handleOpenModal = () => {
-        dispatch(updateClassSelected(row.original.classId));
-        setIsModalOpen(true);
-      };
-
-      const handleCloseModal = () => {
-        dispatch(fetchClassesData(institution.id, initialPage, courseId));
-        setIsModalOpen(false);
-      };
-
       if (row.values.instructors?.length > 0) {
         return (
           <ul className="instructors-list mb-0">
@@ -57,17 +43,9 @@ const columns = [
       }
 
       return (
-        <>
-          <Button onClick={handleOpenModal} className="button-assign">
-            <i className="fa fa-plus mr-2" />
-            Assign
-          </Button>
-          <AssignInstructors
-            isOpen={isModalOpen}
-            close={handleCloseModal}
-            getClasses={false}
-          />
-        </>
+        <span className="text-danger">
+          Unassigned
+        </span>
       );
     },
   },
@@ -110,6 +88,80 @@ const columns = [
     Header: 'End date',
     accessor: 'endDate',
     Cell: ({ row }) => (row.values.endDate ? formatUTCDate(row.values.endDate, 'MM/dd/yy') : '-'),
+  },
+  {
+    Header: '',
+    accessor: 'courseName',
+    cellClassName: 'dropdownColumn',
+    disableSortBy: true,
+    Cell: ({ row }) => {
+      const {
+        masterCourseName,
+        masterCourseId,
+        classId,
+        className,
+        startDate,
+        endDate,
+        minStudentsAllowed,
+        maxStudents,
+      } = row.original;
+
+      const [isOpenModal, openModal, closeModal] = useToggle(false);
+
+      return (
+        <Dropdown className="dropdowntpz">
+          <Dropdown.Toggle
+            id="dropdown-toggle-with-iconbutton"
+            as={IconButton}
+            src={MoreHoriz}
+            iconAs={Icon}
+            variant="primary"
+            data-testid="droprown-action"
+            alt="menu for actions"
+          />
+          <Dropdown.Menu>
+            <Dropdown.Item
+              href={`${getConfig().LEARNING_MICROFRONTEND_URL}/course/${classId}/home`}
+              target="_blank"
+              rel="noreferrer"
+              className="text-truncate text-decoration-none custom-text-black"
+            >
+              <i className="fa-regular fa-eye mr-2 mb-1" />
+              View class content
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <Link
+                to={`/manage-instructors/${masterCourseName}/${row.values.className}?classId=${classId}`}
+                className="text-truncate text-decoration-none custom-text-black"
+              >
+                <i className="fa-regular fa-chalkboard-user mr-2 mb-1" />
+                Manage Instructors
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Item onClick={openModal}>
+              <i className="fa-solid fa-pencil mr-2 mb-1" />
+              Edit Class
+            </Dropdown.Item>
+            <AddClass
+              isOpen={isOpenModal}
+              onClose={closeModal}
+              courseInfo={{
+                masterCourseName,
+                masterCourseId,
+                classId,
+                className,
+                startDate,
+                endDate,
+                minStudents: minStudentsAllowed,
+                maxStudents,
+              }}
+              isCoursePage
+              isEditing
+            />
+          </Dropdown.Menu>
+        </Dropdown>
+      );
+    },
   },
 ];
 
